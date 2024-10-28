cc537bbb7b14e00f4ff82aff89898f0dfcdf68cb
Boubacar-bah <boubacarbahbou6444@gmail.com>
2024-10-28 00:01:35 +0000

    update build for gh-pages

--- src/index.js
+++ src/index.js
@@ -11,60 +11,6 @@ import Appels from './parts/Appels';
 import Header from './parts/Header';
 import Fixed from './parts/FixedButton';
 
-/*import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from './pages/Blogs';
-import Formulaire from './pages/Formulaire';
-import Lists from './pages/Lists';
-import Car from "./pages/Application";
-
-
-function Auto(props) {
-    return <li>Je suis une { props.mark }</li>
-}
-function Garage() {
-    const cars = [
-        {id: 1, mark: 'Mercedes'},
-        {id: 2, mark: 'Toyota'},
-        {id: 3, mark: 'Volvo'}
-    ]
-    return (
-        <>
-            <h1>Voitures ! Présentez-vous !</h1>
-            <ul>
-                {cars.map((car) => <Auto key={car.id} mark={car.mark} />)}
-            </ul>
-        </>
-    )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(< Garage />);
-
-
-
-export default function App() {
-    return ( <div>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path='blogs' element={<Blogs />} />
-                    <Route path='formulaire' element={<Formulaire />} />
-                    <Route path='*' element={<Lists />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-        </div>
-    )
-}
-
-const roote = ReactDOM.createRoot(document.getElementById('root1'));
-roote.render( <App />)
-const root2 = ReactDOM.createRoot(document.getElementById("root3"));
-root2.render(<Car />)
-*/
-
 
 
 const App = () => {
